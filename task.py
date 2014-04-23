ad7aced4f63f33f4c4273300a3421c33bc0f65a6
?ngel ?lvarez <angel@nan-tic.com>
2014-04-23 10:04:38 +0200

    add type phase and remove unnecessary dependencies

--- task.py
+++ task.py
@@ -1,7 +1,7 @@
-#This file is part of Tryton.  The COPYRIGHT file at the top level of
-#this repository contains the full copyright notices and license terms.
+# This file is part of Tryton.  The COPYRIGHT file at the top level of
+# this repository contains the full copyright notices and license terms.
 from trytond.model import ModelView, ModelSQL, fields
-from trytond.pool import PoolMeta
+from trytond.pool import Pool, PoolMeta
 from trytond.pyson import Eval
 
 __all__ = ['TaskPhase', 'Work']
@@ -13,7 +13,10 @@ class TaskPhase(ModelSQL, ModelView):
     __name__ = 'project.work.task_phase'
 
     name = fields.Char('Name', required=True, select=True)
+    type = fields.Selection([(None, ''), ('initial', 'Initial'),
+        ('final', 'Final')], 'Type')
     comment = fields.Text('comment')
+
 
 class Work:
     __name__ = 'project.work'
@@ -24,7 +27,34 @@ class Work:
             'invisible': Eval('type') != 'task',
             }, depends=['type'])
 
+    @classmethod
+    def __setup__(cls):
+        super(Work, cls).__setup__()
+        cls._error_messages.update({
+                'invalid_phase': ('Task "%(work)s" can not be closed on '
+                    'phase "%s(phase)s". Closed states:"%(states)s"'),
+                })
 
+    @staticmethod
+    def default_task_phase():
+        Phase = Pool().get('project.work.task_phase')
+        phase = Phase.search([('type', '=', 'initial')])
+        return phase and phase[0].id
 
+    def get_closed_states(self):
+        return ['done']
 
+    def check_phase(self):
+        if self.state in self.get_closed_states() and \
+                self.task_phase and self.task_phase.type != 'final':
+            self.raise_user_error('invalid_phase', {
+                'work': self.rec_name,
+                'phase': self.task_phase,
+                'states': self.get_closed_states(),
+            })
 
+    @classmethod
+    def validate(cls, works):
+        super(Work, cls).validate(works)
+        for work in works:
+            work.check_phase()
