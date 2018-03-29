34c97e542f95382cbb2223e7d3852857fe288f48
Albert Cervera i Areny <albert@nan-tic.com>
2018-03-29 23:22:51 +0200

    Add/remove missing views and fix test.

--- tests/test_project_phase.py
+++ tests/test_project_phase.py
@@ -3,32 +3,11 @@
 import unittest
 import trytond.tests.test_tryton
 from trytond.tests.test_tryton import ModuleTestCase
-from trytond.tests.test_tryton import POOL, DB_NAME, USER, CONTEXT
-from trytond.transaction import Transaction
 
 
 class TestCase(ModuleTestCase):
     'Test module'
     module = 'project_phase'
-
-    def setUp(self):
-        super(TestCase, self).setUp()
-        self.task_phase = POOL.get('project.work.task_phase')
-
-    def test0010_create_phases(self):
-        with Transaction().start(DB_NAME, USER,
-                context=CONTEXT) as transaction:
-            #This is needed in order to get default values for other test
-            #executing in the same database
-            self.task_phase.create([{
-                    'name': 'Initial',
-                    'type': 'initial',
-                    }, {
-                    'name': 'Final',
-                    'type': 'final',
-                    }])
-            transaction.cursor.commit()
-
 
 def suite():
     suite = trytond.tests.test_tryton.suite()
